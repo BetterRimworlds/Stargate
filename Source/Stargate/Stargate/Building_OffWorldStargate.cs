6521ead13f72edd3d759257bd04314cc053589d2
Theodore R. Smith <hopeseekr@gmail.com>
2021-07-21 05:59:56 -0600

    Disorient, sicken or kill nearly every pawn on the map after activating an Transdimensional Stargate.

--- Source/Stargate/Stargate/Building_OffWorldStargate.cs
+++ Source/Stargate/Stargate/Building_OffWorldStargate.cs
@@ -95,7 +95,7 @@ namespace Enhanced_Development.Stargate
 
         #endregion
 
-        private void CauseHeartAttack(Pawn pawn)
+        public void CauseHeartAttack(Pawn pawn)
         {
             HediffDef heartAttack = HediffDef.Named("HeartAttack");
 
@@ -104,9 +104,20 @@ namespace Enhanced_Development.Stargate
             pawn.health.AddHediff(heartAttack, heart, null);
         }
 
-        private void CauseSedation(Pawn pawn)
+        private static void CauseSedation(Pawn pawn)
         {
             pawn.health.AddHediff(HediffDefOf.Anesthetic, null, null);
+        }
+
+        private void CauseDisorientation(Pawn pawn)
+        {
+            pawn.health.AddHediff(HediffDefOf.PsychicHangover, null, null);
+            pawn.health.AddHediff(HediffDefOf.CryptosleepSickness, null, null);
+        }
+
+        private static void CauseCatatonia(Pawn pawn)
+        {
+            pawn.health.AddHediff(HediffDefOf.CatatonicBreakdown, null, null);
         }
 
         private void AddPsionicShock(Pawn pawn)
@@ -132,21 +143,50 @@ namespace Enhanced_Development.Stargate
             else if (psychicSensitivity == -1)
             {
                 shouldGiveHeartAttack = false;
+                CauseDisorientation(pawn);
             }
             // If they're Psychically Sensitive, make sure they're passed out for a few hours.
             else if (psychicSensitivity == 1)
             {
                 shouldSedate = true;
             }
-            // If they're Psychically Hypersensitive, unfortunately, it will mean instant death :-(
+            // If they're Psychically Hypersensitive, unfortunately, it will mean instant death or catatonia :-(
             else if (psychicSensitivity >= 2)
             {
-                if (pawn.IsColonist)
+                bool shouldKill = rand.Next(1, 13) >= 6;
+                if (shouldKill == true)
                 {
-                    Messages.Message(pawn.Name.ToStringShort + " was psychically supersensitive and died because of the psionic blast.", MessageTypeDefOf.ThreatSmall);
-                }
+                    if (pawn.IsColonist)
+                    {
+                        Messages.Message(pawn.Name.ToStringShort + " was psychically supersensitive and died because of the psionic blast.", MessageTypeDefOf.ThreatSmall);
+                    }
 
-                HealthUtility.DamageUntilDead(pawn);
+                    HealthUtility.DamageUntilDead(pawn);
+                }
+                else
+                {
+                    CauseCatatonia(pawn);
+                }
+            }
+            
+            // If they have a Bionic Heart, then skip but give them bad food poisoning instead.
+            // bool hasBionicHeart = pawn.RaceProps.body.AllParts.Exists(bpr =>
+            // {
+            //     return bpr.def.defName == "Heart" && (
+            //         bpr.def.label.Contains("bionic") ||
+            //         bpr.def.label.Contains("arcotech")
+            //     );
+            // });
+            bool hasBionicHeart =
+                pawn.health.hediffSet.HasHediff(DefDatabase<HediffDef>.GetNamedSilentFail("BionicHeart"));
+            if (pawn.IsColonist)
+            {
+                Log.Error("Does " + pawn.Name + " have a bionic heart? " + hasBionicHeart);
+            }
+
+            if (shouldGiveHeartAttack != false && hasBionicHeart)
+            {
+                shouldGiveHeartAttack = false;
             }
 
             Hediff shock = HediffMaker.MakeHediff(HediffDefOf.PsychicShock, pawn, null);
@@ -165,21 +205,12 @@ namespace Enhanced_Development.Stargate
             if (shouldSedate == null)
             {
                 int likelihood = rand.Next(1, 11);
-                if (pawn.Name?.ToStringShort != null)
-                {
-                    Log.Message(pawn.Name.ToStringShort + " should sedate? " + likelihood);
-                }
-                else
-                {
-                    Log.Message(pawn.def.label + " should sedate? " + likelihood);
-                }
-
                 shouldSedate = likelihood >= 6;
             }
 
             if (shouldSedate == true)
             {
-                this.CauseSedation(pawn);
+                CauseSedation(pawn);
             }
 
             DamageInfo psionicIntensity = new DamageInfo(DamageDefOf.Stun, 50);
