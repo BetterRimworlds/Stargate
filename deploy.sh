20097022c5ec52dd5d232a551996f3d5fd555580
Theodore R. Smith <hopeseekr@gmail.com>
2023-05-22 20:54:13 -0500

    [m] Updated the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-rsync -rcv --delete-after README.md Stargate/About Stargate/Defs /c/RimWorld1-2-2900Win64/Mods/Stargate/
-unix2dos /c/RimWorld1-2-2900Win64/Mods/Stargate/README.md
+unix2dos README.md
+rsync -rcv --delete-after README.md Stargate/About Stargate/Defs Stargate/Textures  /rimworld/1.2/Mods/Stargate/
+rsync -rcv --delete-after README.md Stargate/About Stargate/Defs Stargate/Textures  /rimworld/1.3/Mods/Stargate/
+rsync -rcv --delete-after README.md Stargate/About Stargate/Defs Stargate/Textures  /rimworld/1.4/Mods/Stargate/
+rsync -rcv --delete-after README.md Stargate/About Stargate/Defs Stargate/Textures  $HOME/.steam/steam/steamapps/common/RimWorld/Mods/Stargate/
+rsync -rcv /rimworld/1.2/Mods/Stargate/1.2 $HOME/.steam/steam/steamapps/common/RimWorld/Mods/Stargate/
+rsync -rcv /rimworld/1.3/Mods/Stargate/1.3 $HOME/.steam/steam/steamapps/common/RimWorld/Mods/Stargate/
+rsync -rcv /rimworld/1.4/Mods/Stargate/1.4 $HOME/.steam/steam/steamapps/common/RimWorld/Mods/Stargate/
+
+rsync -rcv --delete-after $HOME/.steam/steam/steamapps/common/RimWorld/Mods/Stargate /rimworld/1.2/Mods
+dos2unix README.md
